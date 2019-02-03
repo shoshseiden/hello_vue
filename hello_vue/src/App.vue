e5c20965cba388e05d0484436fef3c899f8ccbc5
Shosh Seiden <shosh.j.seiden@gmail.com>
2019-02-03 14:31:15 -0700

    Stripped default contents from vue

--- hello_vue/src/App.vue
+++ hello_vue/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHello/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHello from './components/MyHello.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyHello
   }
 }
 </script>
